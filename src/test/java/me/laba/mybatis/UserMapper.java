6d82b52f5eeb6c1c5510085f21caacd5a9a631bb
eric.zhang <eric.zhang@wingconn.com>
2020-10-30 17:54:45 +0800

    update test package name fro myself

---
+++ src/test/java/me/laba/mybatis/UserMapper.java
@@ -0,0 +1,22 @@
+package me.laba.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户操作Mapper
+ *
+ * @author JKong
+ * @version v0.0.1
+ * @date 2020/6/24 12:45.
+ */
+@Mapper
+public interface UserMapper {
+
+    /**
+     * 根据ID获取用户信息
+     *
+     * @param id 用户Id
+     * @return 用户信息
+     */
+    User findUserById(Integer id);
+}
---
+++ src/test/java/me/laba/mybatis/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 主要关注mybatis使用流程，测试类执行步骤：
+ * 1. 在MySQL数据库中执行SQL脚本（src/test/java/resources/user.sql）
+ * 2. 修改mybatis配置文件的数据源配置信息（src/test/java/resources/mybatis.xml）
+ *
+ * @author JKong
+ * @version v0.0.1
+ * @date 2020-06-23 23:24.
+ */
+package me.jkong.mybatis;
